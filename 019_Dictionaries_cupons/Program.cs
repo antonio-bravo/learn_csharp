33b8d76d2c897234040bf9680742d77ffb238715
Antonio Bravo <bravo.antonio@gmail.com>
2023-08-28 22:44:10 +0200

    Hash-cupons

---
+++ 019_Dictionaries_cupons/Program.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        HashSet<int> usedCoupons = new HashSet<int>(); 
+
+        do 
+        { 
+            Console.Write("Enter the coupon number or [l] to list used: "); 
+            string couponString = Console.ReadLine(); 
+            
+            if (int.TryParse(couponString, out int coupon)) 
+            { 
+                if (usedCoupons.Contains(coupon)) 
+                { 
+                    Console.ForegroundColor = ConsoleColor.Red; 
+                    Console.WriteLine("It has been already used :-("); 
+                    Console.ForegroundColor = ConsoleColor.Gray; 
+                } 
+                else 
+                { 
+                    usedCoupons.Add(coupon); 
+                    Console.ForegroundColor = ConsoleColor.Green; 
+                    Console.WriteLine("Thank you! :-)"); 
+                    Console.ForegroundColor = ConsoleColor.Gray; 
+                } 
+            } 
+            else 
+            { 
+                break; 
+            } 
+        } 
+        while (true); 
+
+        Console.WriteLine(); 
+        Console.WriteLine("A list of used coupons:"); 
+        foreach (int coupon in usedCoupons) 
+        { 
+            Console.WriteLine(coupon); 
+        } 
+    }
+}
