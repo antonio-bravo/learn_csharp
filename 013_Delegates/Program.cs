833359f1147dfffb345b548c78f3cfd30c9a5c66
Antonio Bravo <bravo.antonio@gmail.com>
2023-08-28 21:38:21 +0200

    Delegate

---
+++ 013_Delegates/Program.cs
@@ -0,0 +1,29 @@
+using System;
+
+delegate double Mean(double a, double b, double c);
+
+class Program
+{
+    static double Harmonic(double a, double b, double c)
+    {
+        return 3 / ((1 / a) + (1 / b) + (1 / c));
+    }
+
+    static void Main(string[] args)
+    {
+        Mean arithmetic = (a, b, c) => (a + b + c) / 3;
+        Mean geometric = delegate (double a, double b, double c)
+        {
+            return Math.Pow(a * b * c, 1 / 3.0);
+        };
+        Mean harmonic = Harmonic;
+
+        double arithmeticResult = arithmetic.Invoke(5, 6.5, 7);
+        double geometricResult = geometric.Invoke(5, 6.5, 7);
+        double harmonicResult = harmonic.Invoke(5, 6.5, 7);
+
+        Console.WriteLine("Arithmetic Mean: " + arithmeticResult);
+        Console.WriteLine("Geometric Mean: " + geometricResult);
+        Console.WriteLine("Harmonic Mean: " + harmonicResult);
+    }
+}
